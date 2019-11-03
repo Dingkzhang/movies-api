9de73cfe1974aad1ee2c58975b1ff0eee020ec33
Dingkzhang <dingkzhang@utexas.edu>
2019-11-02 21:47:54 -0500

    adding movies-api setup

---
+++ movies/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Movie(models.Model):
+	name = models.CharField(max_length=100)
+	year_of_release = models.PositiveSmallIntegerField()
+	
+	def __str__(self):
+		return self.name
